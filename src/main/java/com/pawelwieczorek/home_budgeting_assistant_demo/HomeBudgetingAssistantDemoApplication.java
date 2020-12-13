3a91b5c98d493dad61046a7e1f8676b2f3c8e997
PawelWieczorek <p.wieczoreknkse@gmail.com>
2020-12-13 08:09:55 +0100

    Iniial commit

---
+++ src/main/java/com/pawelwieczorek/home_budgeting_assistant_demo/HomeBudgetingAssistantDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pawelwieczorek.home_budgeting_assistant_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudgetingAssistantDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBudgetingAssistantDemoApplication.class, args);
+	}
+
+}
